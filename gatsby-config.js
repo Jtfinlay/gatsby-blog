b7de15d315030ec258a14f0aa813e4aec586ebfc
James Finlay <jtfinlay@ualberta.ca>
2018-08-24 22:59:27 -0700

    Header is looking good

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+    siteMetadata: {
+        title: 'James Finlay - Coder, Cook',
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sass'
+    ],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,46 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faRss } from '@fortawesome/free-solid-svg-icons'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+import './header.scss';
+
+const Header = () => (
+  <div className='header_block'>
+    <div className='header'>
+            <ul className='navMenu'>
+                <li className='navMenuItem'>
+                    <Link to='/'>James Finlay</Link>
+                </li>
+                <li className='navMenuItem'>
+                    <Link to='/about'>about</Link>
+                </li>
+            </ul>
+            <ul className='socialIcons'>
+                <li className='socialIcon_link'>
+                    <a href='https://twitter.com/JtFinlay' className='fa twitter'>
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                </li>
+                <li className='socialIcon_link'>
+                    <a href='https://github.com/Jtfinlay' className='fa github'>
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                </li>
+                <li className='socialIcon_link'>
+                    <a href='https://www.linkedin.com/in/james-finlay-b204a731/' className='fa linkedin'>
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                </li>
+                <li className='socialIcon_link'>
+                    <a href='http://feeds.feedburner.com/jtfinlay' className='fa rss'>
+                        <FontAwesomeIcon icon={faRss} />
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
-)
+);
 
-export default Header
+export default Header;
